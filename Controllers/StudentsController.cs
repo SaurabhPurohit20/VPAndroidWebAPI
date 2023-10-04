be9d55d42c42f229995ef7af60cc0733d6aad9d0
master <saurabhpurohit20@gmail.com>
2023-10-04 14:12:23 +0530

    Linq added

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+	public class StudentsController : ApiController
+	{
+		static string cs = ConfigurationManager.ConnectionStrings["db_studentsConnectionString"].ConnectionString;
+		StudentsDataClassesDataContext db = new StudentsDataClassesDataContext(cs);
+
+		[HttpGet]
+		[Route("api/add_student")]
+		public IHttpActionResult demo([FromUri] string name)
+		{
+			db.tbl_student2s.InsertOnSubmit(new tbl_student2
+			{
+				name= name
+			});
+			db.SubmitChanges();
+
+			var v = "Name: "+name+" Added Successfully";
+			return Json(v);
+		}
+		[HttpGet]
+		[Route("api/get_students")]
+		public IHttpActionResult getStudents()
+		{
+			var v=db.tbl_student2s;
+			return Json(v);
+		}
+	}
+}
